5c828d643464d93e93ea68dbbc2e70f436a3220e
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2022-11-28 23:12:44 +0545

    app update

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def load():
     #x=5
     url='https://election.ekantipur.com/?lng=eng'
     web_final.load_update(url)
-    return render_template('index.html', name = 'Federal Parliment Update', url ='/static/images/new_plot.png')
+    return render_template('index.html', name = 'Federal Parliment Update', url ='/static/images/new_plot.png', 
+            url1='/static/images/provincial_plot.png', url2 ='samanu_plot.png')
 
 if __name__=="__main__":
     app.run(debug=True)
